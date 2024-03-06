9b1d225de1aa82ee5b84982b5baacf716d808bb4
JustAGuyNamedCy <cypabeye@gmail.com>
2024-03-06 14:35:34 -0500

    SceneManagement + Board Border

---
+++ ARChessProj_GAME399/Assets/Scripts/SceneManagementStuff.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagementStuff : MonoBehaviour
+{
+    public void SceneTransitioning(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID, LoadSceneMode.Single);
+    }
+}
