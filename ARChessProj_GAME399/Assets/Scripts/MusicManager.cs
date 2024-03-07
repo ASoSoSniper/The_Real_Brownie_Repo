314d6c4fd40b4e2f3ad26d13bf5d3aaaa18449ac
JustAGuyNamedCy <cypabeye@gmail.com>
2024-03-07 18:38:29 -0500

    Music and Stuff

--- ARChessProj_GAME399/Assets/Scripts/MusicManager.cs
+++ ARChessProj_GAME399/Assets/Scripts/MusicManager.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
+    public AudioClip[] musicClips;
+    private AudioSource musicSource;
+
+    private float currClipLength;
+    private int currClipIndex = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        musicSource = GetComponent<AudioSource>();
+        Invoke("PlayNextSong", currClipLength);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void PlayNextSong()
+    {
+        musicSource.clip = musicClips[currClipIndex];
+        currClipLength = musicClips[currClipIndex].length;
+        musicSource.Play();
+        currClipIndex++;
+
+        if (currClipIndex >= musicClips.Length) currClipIndex = 0;        
+
+        Invoke("PlayNextSong", musicClips[currClipIndex].length);
     }
 }
